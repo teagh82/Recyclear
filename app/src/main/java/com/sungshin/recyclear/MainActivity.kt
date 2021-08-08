e106610d3010670c836aabda7b44f9e3d61f1a3e
teagh82 <teagh8237@gmail.com>
2021-08-08 23:14:37 +0900

    [feat] Add button events & Add detail view

--- app/src/main/java/com/sungshin/recyclear/MainActivity.kt
+++ app/src/main/java/com/sungshin/recyclear/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.sungshin.recyclear
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sungshin.recyclear.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        onClickBtns()
+    }
+
+    fun onClickBtns(){
+        binding.buttonMainCan.setOnClickListener {
+            val intent = Intent(this@MainActivity, CanActivity::class.java)
+            startActivity(intent)
+        }
+        binding.buttonMainMetal.setOnClickListener {
+            val intent = Intent(this@MainActivity, MetalActivity::class.java)
+            startActivity(intent)
+        }
+        binding.buttonMainPaper.setOnClickListener {
+            val intent = Intent(this@MainActivity, PaperActivity::class.java)
+            startActivity(intent)
+        }
+        binding.buttonMainPet.setOnClickListener {
+            val intent = Intent(this@MainActivity, PetActivity::class.java)
+            startActivity(intent)
+        }
+        binding.buttonMainVinyl.setOnClickListener {
+            val intent = Intent(this@MainActivity, VinylActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
