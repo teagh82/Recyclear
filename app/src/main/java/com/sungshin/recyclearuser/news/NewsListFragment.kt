2c56cd817bd7af0fac0e5cba45de363b7894d560
teagh82 <teagh8237@gmail.com>
2021-08-14 05:01:22 +0900

    [feature] Modify recycler view & news view data

--- app/src/main/java/com/sungshin/recyclearuser/news/NewsListFragment.kt
+++ app/src/main/java/com/sungshin/recyclearuser/news/NewsListFragment.kt
@@ -33,6 +33,36 @@ class NewsListFragment : Fragment() {
         newsListAdapter.newsList.addAll(
             listOf<NewsListInfo>(
                 NewsListInfo(
+                    news_title = "송가인-서경덕, '독립유공자 후손 주거환경 개선' 캠페인",
+                    news_date = "2021.08.13",
+                    news_link = "https://www.yna.co.kr/view/AKR20210813014200371?input=1195m"
+                ),
+                NewsListInfo(
+                    news_title = "'환경 재앙' 그리스 산불…총리 “기후 위기 대응하겠다“",
+                    news_date = "2021.08.13",
+                    news_link = "https://www.news1.kr/articles/?4403007"
+                ),
+                NewsListInfo(
+                    news_title = "국민 97.8% “플라스틱 폐기물 환경 오염 심각”",
+                    news_date = "2021.08.11",
+                    news_link = "https://www.seoul.co.kr/news/newsView.php?id=20210811500061&wlog_tag3=naver"
+                ),
+                NewsListInfo(
+                    news_title = "자연·수행환경 파괴 장안사 인근 폐기물 매립장 반대",
+                    news_date = "2021.08.13",
+                    news_link = "https://www.pressian.com/pages/articles/2021081317003498717?utm_source=naver&utm_medium=search#0DKU"
+                ),
+                NewsListInfo(
+                    news_title = "포항시, 환경정보 제공 강화해 시민들 불안감 해소",
+                    news_date = "2021.08.13",
+                    news_link = "http://www.ecolaw.co.kr/news/articleView.html?idxno=93345"
+                ),
+                NewsListInfo(
+                    news_title = "대구환경청, 포항제철소에서 자원순환 홍보 전시회 17일까지 개최",
+                    news_date = "2021.08.13",
+                    news_link = "https://www.news1.kr/articles/?4403326"
+                ),
+                NewsListInfo(
                     news_title = "고양시, 섬유 폐기물을 친환경 자원으로 바꾼다",
                     news_date = "2021.08.10",
                     news_link = "http://www.ecolaw.co.kr/news/articleView.html?idxno=93345"
@@ -43,34 +73,14 @@ class NewsListFragment : Fragment() {
                     news_link = "https://www.beopbo.com/news/articleView.html?idxno=301995"
                 ),
                 NewsListInfo(
-                    news_title = "고양시, 섬유 폐기물을 친환경 자원으로 바꾼다",
-                    news_date = "2021.08.10",
-                    news_link = "http://www.ecolaw.co.kr/news/articleView.html?idxno=93345"
-                ),
-                NewsListInfo(
-                    news_title = "자연·수행환경 파괴 장안사 인근 폐기물 매립장 반대",
+                    news_title = "인공지능팩토리, AI로 지구환경 문제 해결 해커톤 주관",
                     news_date = "2021.08.13",
-                    news_link = "https://www.beopbo.com/news/articleView.html?idxno=301995"
+                    news_link = "https://zdnet.co.kr/view/?no=20210813090918"
                 ),
                 NewsListInfo(
-                    news_title = "고양시, 섬유 폐기물을 친환경 자원으로 바꾼다",
-                    news_date = "2021.08.10",
-                    news_link = "http://www.ecolaw.co.kr/news/articleView.html?idxno=93345"
-                ),
-                NewsListInfo(
-                    news_title = "자연·수행환경 파괴 장안사 인근 폐기물 매립장 반대",
-                    news_date = "2021.08.13",
-                    news_link = "https://www.beopbo.com/news/articleView.html?idxno=301995"
-                ),
-                NewsListInfo(
-                    news_title = "고양시, 섬유 폐기물을 친환경 자원으로 바꾼다",
-                    news_date = "2021.08.10",
-                    news_link = "http://www.ecolaw.co.kr/news/articleView.html?idxno=93345"
-                ),
-                NewsListInfo(
-                    news_title = "자연·수행환경 파괴 장안사 인근 폐기물 매립장 반대",
-                    news_date = "2021.08.13",
-                    news_link = "https://www.beopbo.com/news/articleView.html?idxno=301995"
+                    news_title = "환경부 '금강 자연성 회복 사업' 시민 설명회…내년 본격 추진",
+                    news_date = "2021.08.12",
+                    news_link = "https://newsis.com/view/?id=NISX20210812_0001546480&cID=10201&pID=10200"
                 )
             )
         )
--- app/src/main/java/com/sungshin/recyclearuser/recycle/RecycleListFragment.kt
+++ app/src/main/java/com/sungshin/recyclearuser/recycle/RecycleListFragment.kt
@@ -33,43 +33,40 @@ class RecycleListFragment : Fragment() {
         recycleListAdapter.recycleList.addAll(
             listOf<RecycleListInfo>(
                 RecycleListInfo(
-                    image = "https://images.squarespace-cdn.com/content/v1/5c8d13427a1fbd35297a9ed1/1584916870771-Y1V8HXYAZAEV75ZYK4DH/reusable+bag+example.png?format=1000w",
-                    content = "비닐은 뽁뽁이 등이 있다..",
+                    image = "https://image.msscdn.net/images/goods_img/20200210/1298440/1298440_1_500.jpg",
+                    content = "비닐은 색상이나 재활용 마크에 관계없이 깨끗이 씻어 투명비닐봉투에 담아 배출해야한다.",
                     title = "비닐은 어떻게?"
                 ),
                 RecycleListInfo(
-                    image = "https://images.squarespace-cdn.com/content/v1/5c8d13427a1fbd35297a9ed1/1584916870771-Y1V8HXYAZAEV75ZYK4DH/reusable+bag+example.png?format=1000w",
-                    content = "비닐은 뽁뽁이 등이 있다..",
-                    title = "비닐은 어떻게?"
+                    image = "https://jmagazine.joins.com/_data/photo/2020/06/3698936108_Sn9C7dOw_1.jpg",
+                    content = "플라스틱류는 용기 안에 들어있는 내용물을 깨끗이 비운 후, 부착되어 있는 상표를 제거하고 뚜껑 등 다른 재질로 되어있는 부분도 분리해야한다.\n가능한 압착하여 버린다.",
+                    title = "플라스틱 분리 배출 방법!"
                 ),
                 RecycleListInfo(
-                    image = "https://images.squarespace-cdn.com/content/v1/5c8d13427a1fbd35297a9ed1/1584916870771-Y1V8HXYAZAEV75ZYK4DH/reusable+bag+example.png?format=1000w",
-                    content = "비닐은 뽁뽁이 등이 있다..",
-                    title = "비닐은 어떻게?"
+                    image = "https://m.economictimes.com/thumb/msid-61941670,width-1200,height-900,resizemode-4,imgsize-25928/better-valuations-of-paper-companies-just-a-matter-of-time.jpg",
+                    content = "비닐 코팅된 표지나 제본 스프링 등은 분리하여 배출해야한다.\n종이컵은 내용물을 비운 뒤 배출해야 한다.",
+                    title = "종이 분리수거 📄"
                 ),
                 RecycleListInfo(
-                    image = "https://images.squarespace-cdn.com/content/v1/5c8d13427a1fbd35297a9ed1/1584916870771-Y1V8HXYAZAEV75ZYK4DH/reusable+bag+example.png?format=1000w",
-                    content = "비닐은 뽁뽁이 등이 있다..",
-                    title = "비닐은 어떻게?"
+                    image = "https://cdn.011st.com/11dims/resize/600x600/quality/75/11src/pd/19/1/6/8/0/7/4/TpQIr/425168074_B.jpg",
+                    content = "내용물을 비운 뒤 배출해야 한다. 뚜껑은 제거해야 한다.",
+                    title = "캔은 어떻게?"
                 ),
                 RecycleListInfo(
-                    image = "https://images.squarespace-cdn.com/content/v1/5c8d13427a1fbd35297a9ed1/1584916870771-Y1V8HXYAZAEV75ZYK4DH/reusable+bag+example.png?format=1000w",
-                    content = "비닐은 뽁뽁이 등이 있다..",
-                    title = "비닐은 어떻게?"
+                    image = "https://gainglobal.kr/data/item/1529372894/thumb-6riw_400x400.jpg",
+                    content = "뚜껑을 제거 후 내용물을 비우고 배출해야 한다. 깨진 유리는 소량이라면 신문지에 싸서 종량제 봉투에 버린다. 양이 많은 경우 특수규격마대를 구매하여 배출한다.",
+                    title = "유리병 잘 버리기"
                 ),
                 RecycleListInfo(
-                    image = "https://images.squarespace-cdn.com/content/v1/5c8d13427a1fbd35297a9ed1/1584916870771-Y1V8HXYAZAEV75ZYK4DH/reusable+bag+example.png?format=1000w",
-                    content = "비닐은 뽁뽁이 등이 있다..",
-                    title = "비닐은 어떻게?"
+                    image = "https://pbs.twimg.com/profile_images/1081070799358849024/4g67jhaR.jpg",
+                    content = "주요 거점(동주민센터, 주택가 골목 등)에 설치된 전용 수거함에 배출한다.",
+                    title = "건전지를 버리는 방법"
                 ),
                 RecycleListInfo(
-                    image = "https://images.squarespace-cdn.com/content/v1/5c8d13427a1fbd35297a9ed1/1584916870771-Y1V8HXYAZAEV75ZYK4DH/reusable+bag+example.png?format=1000w",
-                    content = "비닐은 뽁뽁이 등이 있다..",
-                    title = "비닐은 어떻게?"
+                    image = "",
+                    content = "",
+                    title = ""
                 ),
-
-
-
             )
         )
         // 데이터 변경되었으니 업데이트해라
