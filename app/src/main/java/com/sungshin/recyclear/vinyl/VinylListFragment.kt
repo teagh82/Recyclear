a9a09e0766b277c69544d0f6d606f3b7cd1c4285
teagh82 <teagh8237@gmail.com>
2021-08-17 17:55:09 +0900

    [feat] Add loading indicator

--- app/src/main/java/com/sungshin/recyclear/vinyl/VinylListFragment.kt
+++ app/src/main/java/com/sungshin/recyclear/vinyl/VinylListFragment.kt
@@ -1,16 +1,22 @@
 package com.sungshin.recyclear.vinyl
 
+import android.graphics.Color
+import android.graphics.drawable.AnimationDrawable
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatDialog
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.sungshin.recyclear.R
 import com.sungshin.recyclear.databinding.FragmentVinylListBinding
 import com.sungshin.recyclear.utils.FirebaseUtil
 import com.sungshin.recyclear.vinyl.vinyllist.VinylListAdapter
@@ -32,6 +38,8 @@ class VinylListFragment : Fragment() {
     var hasVinyl3: Boolean = false
     var hasVinyl4: Boolean = false
 
+    private lateinit var progressDialog: AppCompatDialog
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -49,6 +57,7 @@ class VinylListFragment : Fragment() {
     }
 
     private fun loadDatas(){
+        progressON()
         val valueEventListener = object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 for (userSnapshot in dataSnapshot.children) {
@@ -199,6 +208,7 @@ class VinylListFragment : Fragment() {
                     binding.constraintlayoutVinylRecycler.visibility = View.GONE
                     binding.constraintlayoutVinylEmpty.visibility = View.VISIBLE
                 }
+                progressOFF()
             }
 
 
@@ -209,5 +219,27 @@ class VinylListFragment : Fragment() {
 
         val classRef = database.reference.child("User")
         classRef.addValueEventListener(valueEventListener)
+    }
+
+    // loading indicator
+    fun progressON(){
+        progressDialog = AppCompatDialog(this.context)
+        progressDialog.setCancelable(false)
+        progressDialog.getWindow()?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        progressDialog.setContentView(R.layout.loading)
+        progressDialog.show()
+        var img_loading_framge = progressDialog.findViewById<ImageView>(R.id.iv_frame_loading)
+        var frameAnimation = img_loading_framge?.getBackground() as AnimationDrawable
+        img_loading_framge?.post(object : Runnable{
+            override fun run() {
+                frameAnimation.start()
+            }
+
+        })
+    }
+    fun progressOFF(){
+        if(progressDialog != null && progressDialog.isShowing()){
+            progressDialog.dismiss()
+        }
     }
 }
